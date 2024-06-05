171bb572bf0d4acc462d3f804e4187130931ef63
llamanati <THELLAMANATI@ICLOUD.COM>
2024-06-05 12:38:39 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: Llamapedia 2.0",
+    pageTitle: "Llamapedia 2.0",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
